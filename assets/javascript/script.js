42eed5706e3bee8143efece829c65f3e6daf52f8
Sean Belverstone <seanbelverstone@gmail.com>
2019-07-26 17:56:20 -0500

    Added contact form layout. Included icons on each part to assist in accessability. Also added a materalize jquery part for the textbox so it resizes dependent on how much text has been entered. Also added a preventDefault() function so if you press enter, it doesnt reload the page.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
+// Mobile responsive navbar
+
+$(document).ready(function() {
     $('.sidenav').sidenav();
   });
+
+
+
+//Text area on contact page
+$('#textarea1').val();
+M.textareaAutoResize($('#textarea1'));
+
+
+$(document).ready(function(event) {
+  event.preventDefault();
+})
