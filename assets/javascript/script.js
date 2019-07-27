2613fecb9a57f4ee85cc700841ae94593fdfd2aa
Sean Belverstone <seanbelverstone@gmail.com>
2019-07-27 16:58:15 -0500

    Added form functionality to the contact page. Made it so when you fill it out, the page loads your email provider and fills the subject and message with the details entered into the form. Going to alter my footer to add my email and telephone number and also make sure the form gets cleared when submit is entered.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,6 +11,55 @@ $('#textarea1').val();
 M.textareaAutoResize($('#textarea1'));
 
 
-$(document).ready(function(event) {
-  event.preventDefault();
-})
+//Checking that the form is complete
+var nameEmailNumber;
+var messageVal;
+
+
+function validateForm() {
+
+  $("input").each(function() {
+
+    if ($(this).val() === '') {
+      event.preventDefault();
+      M.toast({html: 'An input field is blank'});
+      nameEmailNumber = false;
+
+    } else {
+      nameEmailNumber = true;
+      details.push($(this).val());
+    }
+
+  });
+
+  $("textarea").each(function() {
+    if ($(this).val() === '') {
+      event.preventDefault();
+      M.toast({html: 'An input fields is blank'});
+      messageVal = false;
+
+    } else {
+      messageVal = true;
+      message = $(this).val();
+    }
+  });
+
+};
+  
+var details = [];
+var message;
+
+  $("#submit").on("click", function(event) {
+    
+    validateForm();
+    if (nameEmailNumber && messageVal === true) {
+      var email = 'seanbelverstone@gmail.com';
+
+      $(this).attr('href', 'mailto:' + email +
+      '?subject=' + details.join(" ") + '&body=' + message);    
+      // console.log(email, subject, emailBody, message);
+
+
+
+  };
+});
